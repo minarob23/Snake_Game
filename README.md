457c964ab3a6a884eb1d986aeb1c871d3dd441dd
minarob23 <menarobir246rm@gmail.com>
2023-06-13 02:03:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project Snake Game used by Java
+To do, Download the game & Enjoy it !!!
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    public Main() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Game());
+
+        setTitle("Snake");
+        setSize(810, 610);
+
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            Main ex = new Main();
+            ex.setVisible(true);
+        });
+    }
+}
